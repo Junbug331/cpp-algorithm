744b8cc127d9ec2fd3eee03d98843ee792e6f7a5
Jun <junbug331@gmail.com>
2020-12-11 20:55:03 +0900

    Added PrimeFactorization() in PrimeNuber project

--- PrimeNumber/main.cpp
+++ PrimeNumber/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h>
+#include <string>
+
 
 using namespace std;
 
@@ -7,14 +9,54 @@ bool isPrime(int n)
 {
     for (int i = 2; i <= n && i*i <= n*n; ++i)
     {
-        if (n % i == 0)
+        if ( i < n && n % i == 0 )
             return false;
     }
     return true;
 }
 
+void PrimeFactorization(int n, int* f, int* exep, int* len)
+{
+    int d = 2;
+    *len = 0;
+    
+    if ( n == 1)
+    {
+        (*len)++;
+        f[*len] = 2;
+        exep[*len] = 0;
+
+        return;
+    }
+
+    while (n > 1 && d*d <= n*n)
+    {
+        int k = 0;
+        while (n % d == 0)
+        {
+            n /= d;
+            k++;
+        }
+        if (k > 0)
+        {
+            f[*len] = d;
+            exep[*len] = k;
+            (*len)++;
+        }
+        d++;
+    }
+
+    if ( n > 1 )
+    {
+        f[*len] = n;
+        exep[*len] = 1;
+        (*len)++;
+    }
+}
+
 int main()
 {
+    cout << "Testing isPrime() " << endl;
     for (int i = 2; i < 20; i++)
     {
         if (isPrime(i))
@@ -24,6 +66,23 @@ int main()
             cout << i << " is not a prime number.\n";
         }
     }
+    cout<<endl;
+
+    cout << "Testing PrimeFactorization() " << endl;
+    cout << "Prime factoriztion of 100 is " << endl;
     
+    int n = 1020;
+    int f[100], exep[100];
+    int len;
+    PrimeFactorization(n, f, exep, &len);
+
+    string result = "";
+    for (int i=0; i<len; i++)
+    {
+        result += to_string(f[i]) + "^" + to_string(exep[i]) + " * ";
+    }
+    result.replace(result.size()-3, 3, "");
+    cout << result << endl;
+
     return 0;
 }
