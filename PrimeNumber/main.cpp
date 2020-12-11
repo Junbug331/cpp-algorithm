8b77f87be4a1018c6f61965494f172b8cef469af
Jun <junbug331@gmail.com>
2020-12-11 19:48:26 +0900

    Optimized isPrime() in PrimeNumber project

--- PrimeNumber/main.cpp
+++ PrimeNumber/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 bool isPrime(int n)
 {
-    for (int i = 2; i <= static_cast<int>(sqrt(n)); ++i)
+    for (int i = 2; i <= n && i*i <= n*n; ++i)
     {
         if (n % i == 0)
             return false;
